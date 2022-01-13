afc0b28efbe9c1492f1b40433ee45a52c031eb9a
Max Pershin <mepershin@gmail.com>
2022-01-13 16:00:47 +0000

    diff augs from original repo

--- src/augment.py
+++ src/augment.py
@@ -1,30 +1,53 @@
 import torch
 import torch.nn as nn
-import kornia
+import torch.nn.functional as F
+
+
+def rand_brightness(x):
+    x = x + (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5)
+    return x
+
+
+def rand_saturation(x):
+    x_mean = x.mean(dim=1, keepdim=True)
+    x = (x - x_mean) * (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) * 2) + x_mean
+    return x
+
+
+def rand_contrast(x):
+    x_mean = x.mean(dim=[1, 2, 3], keepdim=True)
+    x = (x - x_mean) * (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) + 0.5) + x_mean
+    return x
+
+
+def rand_translation(x, ratio=0.125):
+    shift_x, shift_y = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
+    translation_x = torch.randint(-shift_x, shift_x + 1, size=[x.size(0), 1, 1], device=x.device)
+    translation_y = torch.randint(-shift_y, shift_y + 1, size=[x.size(0), 1, 1], device=x.device)
+    grid_batch, grid_x, grid_y = torch.meshgrid(
+        torch.arange(x.size(0), dtype=torch.long, device=x.device),
+        torch.arange(x.size(2), dtype=torch.long, device=x.device),
+        torch.arange(x.size(3), dtype=torch.long, device=x.device),
+    )
+    grid_x = torch.clamp(grid_x + translation_x + 1, 0, x.size(2) + 1)
+    grid_y = torch.clamp(grid_y + translation_y + 1, 0, x.size(3) + 1)
+    x_pad = F.pad(x, [1, 1, 1, 1, 0, 0, 0, 0])
+    x = x_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2)
+    return x
 
 
 class DiffAugment(nn.Module):
 
     def __init__(self):
         super().__init__()
-
-        self._layers = nn.Sequential(
-                kornia.augmentation.ColorJitter(
-                        p=1.0,
-                        brightness=0.5,
-                        contrast=0.5,
-                        saturation=0.5,
-                        hue=0.0,
-                    ),
-                kornia.augmentation.RandomAffine(
-                        p=0.5,
-                        degrees=0,
-                        scale=(0.7, 1.3),
-                        translate=(0.125, 0.125),
-                        shear=(0, 0),
-                        padding_mode='zeros',
-                    ),
-            )
+        self._augs = [
+                rand_brightness,
+                rand_saturation,
+                rand_contrast,
+                rand_translation,
+            ]
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
-        return self._layers(images)
+        for aug in self._augs:
+            images = aug(images)
+        return images.contiguous()
